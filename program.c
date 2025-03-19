ca4d190acc76d9314251e13230cbba31b571325b
eddyjiang <133807074+eddyjiang@users.noreply.github.com>
2025-03-19 18:02:39 -0500

    Create program.c

---
+++ program.c
@@ -0,0 +1,27 @@
+/* This program is compiled to use the custom heap allocator
+ * when allocating memory. Write any program that allocates memory
+ * using mymalloc, myrealloc, and myfree instead of malloc, realloc,
+ * and free.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "allocator.h"
+#include "segment.h"
+
+#define HEAP_SIZE 1L << 32
+
+bool initialize_heap_allocator() {
+    init_heap_segment(HEAP_SIZE);
+    return myinit(heap_segment_start(), heap_segment_size());
+}
+
+int main(int argc, char *argv[]) {
+    if (!initialize_heap_allocator()) {
+        return 1;
+    }
+
+    // optionally write your code here!
+    
+    return 0;
+}
